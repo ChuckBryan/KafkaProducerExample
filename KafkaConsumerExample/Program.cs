bb30ce04bd30d95c73f09474e9bee7975048d4cc
Chuck Bryan <cbryan@marathonus.com>
2023-12-22 11:06:29 -0500

    Add KafkaConsumerExample project and Program.cs
    The new project KafkaConsumerExample has been added, which includes a basic configuration for a .NET Core console application that will be used in interacting with Kafka. Additionally, a Program.cs file has been created within this project, containing the main logic for a Kafka consumer application, including consuming messages and handling exceptions.

---
+++ KafkaConsumerExample/Program.cs
@@ -0,0 +1,65 @@
+﻿using Confluent.Kafka;
+
+string groupId = args.Length > 0 ? args[0] : "default-group";
+
+// this can be loaded externally
+/*
+services.Configure<ConsumerConfig> (
+    hostContext.Configuration.GetSection("Consumer")
+);
+*/
+
+var config = new ConsumerConfig
+{ 
+    BootstrapServers = "localhost:9092",
+    ClientId = "ConsumerExample",
+    GroupId = groupId,
+    AutoOffsetReset = AutoOffsetReset.Earliest,
+    EnableAutoCommit = true
+    // Authentication methods...
+};
+
+// Note: in this example, I am just sending over a double. However, if it was a complex
+// type, you would want to deserialize it with json
+
+/*var consumer = new ConsumerBuilder<string, Biometrics>(config).SetValueDeserializer(
+        new JsonDeserializer<Biometrics>().AsSyncOverAsync()
+    )
+    .Build();*/
+
+using var c = new ConsumerBuilder<string, double>(config).Build();
+
+// subscribe to the topic
+c.Subscribe("myorders");
+
+CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+
+var cancellationToken = cancellationTokenSource.Token;
+
+Console.CancelKeyPress += (_, e) => {
+    e.Cancel = true;
+    cancellationTokenSource.Cancel();
+};
+
+try
+{
+    while (!cancellationToken.IsCancellationRequested)
+    {
+        try
+        {
+            var cr = c.Consume(cancellationToken);
+            Console.WriteLine($"Received message with key: {cr.Message.Key} and value: {cr.Message.Value}");
+            Console.WriteLine($"It comes from partition: {cr.Partition}");
+            
+            Thread.Sleep(1000);
+        }
+        catch (ConsumeException e)
+        {
+            Console.WriteLine($"Error occurred: {e.Error.Reason}");
+        }
+    }
+}
+catch (OperationCanceledException)
+{
+    c.Close();
+}
